755515e926f9d607e6b6257549e81ddab3cb93c9
Ken Moini <ken@kenmoini.com>
2019-12-13 08:06:20 -0600

    update example vars with LDAP bits

--- example.var.sh
+++ example.var.sh
@@ -27,6 +27,15 @@ POSTGRES_DATABASE="tower"
 RABBITMQ_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 RABBITMQ_ERLANG_COOKIE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
+CONFIGURE_TOWER_LDAP="false"
+TOWER_LDAP_DOMAIN_REALM="EXAMPLE.COM"
+TOWER_LDAP_SERVER_URI="ldaps://idm.example.com:636"
+TOWER_LDAP_BIND_DN="cn=Directory Manager"
+TOWER_LDAP_BIND_PASSWORD=""
+TOWER_LDAP_USER_ATTR_MAP='{"first_name": "givenName", "last_name": "sn", "email": "mail"}'
+TOWER_LDAP_GROUP_TYPE="NestedMemberDNGroupType"
+TOWER_LDAP_GROUP_TYPE_PARAMS='{"name_attr": "cn", "member_attr": "member"}'
+
 ## Do not edit past this line
 
 INTERACTIVE="false"
@@ -37,3 +46,10 @@ fi
 if [ $OCP_AUTH_TYPE == "token" ]; then
     OCP_AUTH="--token=$OCP_TOKEN"
 fi
+
+TOWER_LDAP_DC_BASE=$(returnDC $TOWER_LDAP_DOMAIN_REALM)
+TOWER_LDAP_USER_SEARCH="[\"cn=groups,"$(echo $TOWER_LDAP_DC_BASE)"\", \"SCOPE_SUBTREE\", \"(uid=%(user)s)\"]"
+TOWER_LDAP_USER_DN_TEMPLATE="uid=%(user)s,cn=users,cn=accounts,${TOWER_LDAP_DC_BASE}"
+TOWER_LDAP_GROUP_SEARCH="[\"cn=groups,cn=accounts,"$(echo $TOWER_LDAP_DC_BASE)"\", \"SCOPE_SUBTREE\", \"(objectClass=groupOfNames)\"]"
+TOWER_LDAP_USER_FLAGS_BY_GROUP="{ \"is_superuser\": [ \"cn=admins,cn=groups,cn=accounts,"$(echo $TOWER_LDAP_DC_BASE)"\" ] }"
+TOWER_LDAP_ORGANIZATION_MAP="{ \"Default\": { \"users\": \"cn=ipausers,cn=groups,cn=accounts,"$(echo $TOWER_LDAP_DC_BASE)"\", \"admins\": \"cn=admins,cn=groups,cn=accounts,"$(echo $TOWER_LDAP_DC_BASE)"\", \"remove_users\": false, \"remove_admins\": false } }"
